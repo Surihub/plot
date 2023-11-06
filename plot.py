40a1684b58ccc5e2d3c1229004699d49bcc537cb
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2023-11-07 01:14:34 +0900

    Update plot.py
    github에서 edited

--- plot.py
+++ plot.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
-import matplotlib.patheffects as path_effects
 import pandas as pd
-import numpy as np
 from statsmodels.graphics.mosaicplot import mosaic  
+import numpy as np
+import koreanize_matplotlib
 
 # Streamlit 앱 제목 설정
 st.title("📊 데이터 시각화 및 분석")
@@ -14,47 +14,54 @@ st.subheader("1️⃣ 데이터 불러오기")
 
 tab1, tab2 = st.tabs(["seaborn 데이터셋", "파일 업로드"])
 
-# 데이터를 불러오는 로직
 with tab1:
     dataset_name = st.text_input('데이터 예시: titanic, tips, taxis, penguins, iris...:')
-    sample_checked = st.checkbox('seaborn 데이터 확인하기')
-
-    if sample_checked:
+    if st.button('seaborn 데이터 불러오기'):
         with st.spinner('샘플 데이터를 불러오는 중 입니다...'):
             try:
                 df = sns.load_dataset(dataset_name)
-                st.write(df.head(3))
-            except:
-                st.write("⚠데이터셋 이름을 다시 확인해주세요!")
+                st.session_state['df'] = df
+                st.write(df.head())
+            except ValueError:
+                st.error("⚠ 데이터셋 이름을 다시 확인해주세요!")
 
 with tab2:
-    st.write("단, 이 방법은 csv 파일만 지원합니다.")
-    custom_data = st.file_uploader("분석하고 싶은 파12일을 업로드해주세요.", type=["csv", "xlsx"])
-    if custom_data:
-        custom_data = pd.read_csv(custom_data, encoding = 'euc-kr')
-        st.session_state['custom_data'] = custom_data
+    uploaded_file = st.file_uploader("분석하고 싶은 파일을 업로드해주세요.", type=["csv", "xlsx"])
+    if st.button('파일 업로드하기'):
+        if uploaded_file:
+            if uploaded_file.name.endswith('.csv'):
+                df = pd.read_csv(uploaded_file)
+            elif uploaded_file.name.endswith('.xlsx'):
+                df = pd.read_excel(uploaded_file)
+            st.session_state['df'] = df
+            st.write(df.head())
+        else:
+            st.error("⚠ 파일을 업로드해주세요!")
 
-    upload_checked = st.checkbox('업로드한 파일 확인하기!')
-    if upload_checked:
-        with st.spinner('중복을 확인하는 중입니다...'):
-            try:
-                st.write(custom_data.head(5))
-                df = custom_data
-            except:
-                st.write("⚠올바른 파일을 업로드하셨는지 확인해주세요!")
-
-try:
-    # 열 이름 가져오기
-    column_names = df.columns.tolist()
-
-    # 열 이름을 리스트로 나열하고 클릭 가능하게 만들기
-    selected_columns = st.multiselect('분석하고자 하는 열을 선택하세요:', column_names, default=column_names)
-    df = df[selected_columns]
-    # 선택된 열의 데이터 표시
+# 데이터가 불러와졌는지 확인
+if 'df' in st.session_state:
+    df = st.session_state['df']
+    
+    # 열 선택
+    st.subheader('2️⃣ 열 선택')
+    if st.checkbox('모든 열 선택하기', key='select_all'):
+        if 'select_all' in st.session_state and st.session_state['select_all']:
+            default_columns = df.columns.tolist()
+        else:
+            default_columns = []
+    else:
+        default_columns = df.columns.tolist() if 'selected_columns' not in st.session_state else st.session_state['selected_columns']
+    
+    selected_columns = st.multiselect('분석하고자 하는 열을 선택하세요:', df.columns.tolist(), default=default_columns)
+    st.session_state['selected_columns'] = selected_columns
 
     if selected_columns:
-        st.write(df.head(3))
-#################################
+        df = df[selected_columns]
+        st.write(df.head())
+
+        # 데이터 유형 설정
+        st.subheader('3️⃣ 데이터 유형 설정')
+        # if st.button('데이터 유형 업데이트'):
         # 각 열의 데이터 유형을 추론하는 함수
         def infer_column_types(df):
             column_types = {}
@@ -66,16 +73,45 @@ try:
             return column_types
 
         column_types = infer_column_types(df)
+        st.session_state['column_types'] = column_types
 
         # 사용자가 각 열의 데이터 유형을 설정할 수 있도록 입력 받기
         user_column_types = {}
-        for column, col_type in column_types.items():
-            user_col_type = st.selectbox(
-                f"Select the column type for '{column}' (Current: {col_type})",
-                ['Numeric', 'Categorical'],
-                key=column
-            )
-            user_column_types[column] = user_col_type
+        options_en = ['Numeric', 'Categorical']
+        # options_kr = {'Numeric': '수치형', 'Categorical': '범주형'}
+        options_kr = ["수치형", "범주형"]
+        options_dic = {'수치형':'Numeric',  '범주형':'Categorical'}
+        # col1, col2 = st.columns(2):
+        
+        # 반반 나눠서 나열
+        col1, col2 = st.columns(2)
+        keys = list(column_types.keys())
+        half = len(keys) // 2 
+
+        dict1 = {key: column_types[key] for key in keys[:half]}
+        dict2 = {key: column_types[key] for key in keys[half:]}
+
+
+        with col1:
+            for column, col_type in dict1.items():
+                default_index = options_en.index(col_type)  # Get the index of the current col_type
+                user_col_type = st.radio(
+                    f"'{column}'의 유형:",
+                    options_kr,
+                    index=default_index,  # Set the default value
+                    key=column
+                )
+                user_column_types[column] = user_col_type
+        with col2:
+            for column, col_type in dict2.items():
+                default_index = options_en.index(col_type)  # Get the index of the current col_type
+                user_col_type = st.radio(
+                    f"'{column}'의 유형:",
+                    options_kr,
+                    index=default_index,  # Set the default value
+                    key=column
+                )
+                user_column_types[column] = user_col_type
 
         # 사용자의 입력에 따라 DataFrame의 열 유형을 변환
         def convert_column_types(df, user_column_types):
@@ -87,126 +123,70 @@ try:
             return df
 
         # 열 유형 변환 실행
-        df = convert_column_types(df, user_column_types)
+        df = convert_column_types(df, user_column_types)            
+        # ... (데이터 유형 변환 코드)
+        st.session_state['df_updated'] = df
+        st.session_state['user_column_types'] = user_column_types
+        st.success('데이터 유형이 업데이트되었습니다.')
+            # st.write(user_column_types)
 
-        # st.write('Data with updated column types:')
-        # st.dataframe(df)
-#################################
+if 'df_updated' in st.session_state:
+    df_updated = st.session_state['df_updated']
+    user_column_types = st.session_state['user_column_types']
+    # 기술통계량 확인
+    st.subheader('4️⃣ 기술통계량 및 자동 데이터 시각화')
 
-# ["기술통계량", "데이터 시각화"]
-    
-    st.subheader('2️⃣ 기술통계량')
-    
-# ########################
-    # 각 열에 대한 기술통계량 또는 빈도표 생성
-    for column, col_type in user_column_types.items():
-        st.write(f"### {column} ({col_type})")
-        if col_type == 'Numeric':
-            st.write(pd.DataFrame(df[column].describe()).T)
-        elif col_type == 'Categorical':
-            st.write(pd.DataFrame(df[column].value_counts()).T.style.background_gradient(axis=1))
-#################################
-    st.subheader('3️⃣ 시각화 : 일변량')
-    # 각 열에 대한 시각화 생성
-    for column, col_type in column_types.items():
-        with st.container():  # st.container를 사용하여 그래프들을 감싼다.
-            with st.expander(f"{column} - {col_type} 데이터 시각화"):
-                # 하나의 figure에 두 개의 subplot을 생성한다.
-                fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    tab1, tab2  = st.tabs(['기술통계량 확인하기', '자동 데이터 시각화'])
+    with tab1:
+        # 각 열에 대한 기술통계량 또는 빈도표 생성
+        for column, col_type in user_column_types.items():
+            st.write(f"**{column}** ({col_type})")
+            if col_type == '수치형':
+                st.write(pd.DataFrame(df_updated[column].describe()).T)
+            elif col_type == '범주형':
+                st.write(pd.DataFrame(df_updated[column].value_counts()).T.style.background_gradient(axis=1))
+    with tab2:
+        n = len(df_updated.columns)
+        total_plots = n * n
+        completed_plots = 0
+        pal = sns.color_palette("Set2")
 
-                if col_type == 'Numeric':
-                    # 첫 번째 subplot에 히스토그램을 그린다.
-                    sns.histplot(df[column], kde=False, ax=ax1)
-                    ax1.set_title(f"Histogram of {column}")
+        # 프로그레스 바 초기화
+        progress_bar = st.progress(0)
 
-                    # 두 번째 subplot에 상자그림을 그린다.
-                    sns.boxplot(x=df[column], ax=ax2)
-                    ax2.set_title(f"Boxplot of {column}")
+        fig, axes = plt.subplots(n, n, figsize=(5 * n, 5 * n))
 
-                elif col_type == 'Categorical':
-                    # 첫 번째 subplot에 막대그래프를 그린다.
-                    sns.countplot(x=df[column], ax=ax1)
-                    ax1.set_title(f"Countplot of {column}")
-
-                    # 두 번째 subplot에 원 그래프를 그린다.
-                    df[column].value_counts().plot(kind='pie', ax=ax2, autopct='%1.1f%%')
-                    ax2.set_title(f"Pie chart of {column}")
-
-                # 그래프를 스트림릿에 표시한다.
-                st.pyplot(fig)
-    st.subheader("test")
-    # 격자의 크기를 정의합니다. n x n 격자
-    n = len(df.columns)
-    fig, axes = plt.subplots(n, n, figsize=(5 * n, 5 * n))
-
-    for i, col1 in enumerate(df.columns):
-        for j, col2 in enumerate(df.columns):
-            ax = axes[i, j]
-            if i != j:
-                # 수치형 * 수치형 = 산점도
-                if column_types[col1] == 'Numeric' and column_types[col2] == 'Numeric':
-                    sns.scatterplot(data=df, x=col1, y=col2, ax=ax)
-                # 범주형 * 수치형 = 상자그림
-                elif column_types[col1] == 'Categorical' and column_types[col2] == 'Numeric':
-                    sns.boxplot(data=df, x=col1, y=col2, ax=ax)
-                # 수치형 * 범주형 = 상자그림 (순서 바꿈)
-                elif column_types[col1] == 'Numeric' and column_types[col2] == 'Categorical':
-                    sns.boxplot(data=df, x=col2, y=col1, ax=ax)
-                # 범주형 * 범주형 = 모자이크플롯
-                elif column_types[col1] == 'Categorical' and column_types[col2] == 'Categorical':
-                    mosaic(df, [col1, col2], ax=ax)
-                # 그래프 제목 설정
-                ax.set_title(f'{col1} vs {col2}')
-            else:
-                # 같은 열의 조합에는 히스토그램 또는 카운트 플롯
-                if column_types[col1] == 'Numeric':
-                    sns.histplot(df[col1], kde=False, ax=ax)
+        for i, col1 in enumerate(df_updated.columns):
+            for j, col2 in enumerate(df_updated.columns):
+                ax = axes[i, j]
+                if i != j:
+                    # 수치형 * 수치형 = 산점도
+                    if user_column_types[col1] == '수치형' and user_column_types[col2] == '수치형':
+                        sns.scatterplot(data=df_updated, x=col1, y=col2, ax=ax, palette = pal)
+                    # 범주형 * 수치형 = 상자그림
+                    elif user_column_types[col1] == '범주형' and user_column_types[col2] == '수치형':
+                        sns.boxplot(data=df_updated, x=col1, y=col2, ax=ax, palette = pal)
+                    # 수치형 * 범주형 = 상자그림 (순서 바꿈)
+                    elif user_column_types[col1] == '수치형' and user_column_types[col2] == '범주형':
+                        sns.boxplot(data=df_updated, x=col2, y=col1, ax=ax, palette = pal)
+                    # 범주형 * 범주형 = 모자이크플롯
+                    elif user_column_types[col1] == '범주형' and user_column_types[col2] == '범주형':
+                        mosaic(df_updated, [col1, col2], ax=ax)
+                    # 그래프 제목 설정
+                    ax.set_title(f'{col1} vs {col2}')
+                    completed_plots += 1
+                    progress_bar.progress((completed_plots / total_plots) )
                 else:
-                    sns.countplot(x=df[col1], ax=ax)
-                # 대각선 그래프 제목 설정
-                ax.set_title(f'Distribution of {col1}')
+                    # 같은 열의 조합에는 히스토그램 또는 카운트 플롯
+                    if user_column_types[col1] == '수치형':
+                        sns.histplot(df_updated[col1], kde=True, ax=ax, color = 'r')
+                    else:
+                        sns.countplot(x=df_updated[col1], ax=ax, palette = pal)
+                    # 대각선 그래프 제목 설정
+                    ax.set_title(f'Distribution of {col1}')
 
-    # 서브플롯들 사이의 여백을 조정
-    plt.tight_layout()
-    st.pyplot(fig)
-
-
-
-    st.subheader('3️⃣ 시각화 : 이변량')
-    # 각 열에 대한 시각화 생성
-    # 모든 열 쌍에 대한 그래프를 그린다
-    for col1 in df.columns:
-        for col2 in df.columns:
-            # 동일한 열을 비교하지 않는다
-            if col1 != col2:
-                # 수치형 * 수치형 = 산점도
-                if column_types[col1] == 'Numeric' and column_types[col2] == 'Numeric':
-                    st.write(f"### {col1} and {col2} - 산점도")
-                    fig, ax = plt.subplots()
-                    sns.scatterplot(data=df, x=col1, y=col2, ax=ax)
-                    st.pyplot(fig)
-
-                # 범주형 * 수치형 = 상자그림
-                elif column_types[col1] == 'Categorical' and column_types[col2] == 'Numeric':
-                    st.write(f"### {col1} and {col2} - 상자그림")
-                    fig, ax = plt.subplots()
-                    sns.boxplot(data=df, x=col1, y=col2, ax=ax)
-                    st.pyplot(fig)
-
-                elif column_types[col1] == 'Numeric' and column_types[col2] == 'Categorical':
-                    st.write(f"### {col2} and {col1} - 상자그림")
-                    fig, ax = plt.subplots()
-                    sns.boxplot(data=df, x=col2, y=col1, ax=ax)
-                    st.pyplot(fig)
-
-                # 범주형 * 범주형 = 모자이크플롯
-                elif column_types[col1] == 'Categorical' and column_types[col2] == 'Categorical':
-                    from statsmodels.graphics.mosaicplot import mosaic  
-                    st.write(f"### {col1} and {col2} - 모자이크플롯")
-                    fig, ax = plt.subplots(figsize=(8, 6))
-                    mosaic(df, [col1, col2], ax=ax)
-                    plt.title(f'Mosaic plot of {col1} and {col2}')
-                    st.pyplot(fig)
-
-except NameError as e:
-    st.warning('데이터를 먼저 입력해주세요. ')
+        # 서브플롯들 사이의 여백을 조정
+        plt.tight_layout()
+        st.pyplot(fig)
+        # 프로그레스 바 완료
+        progress_bar.empty()
